e4bbb982d0b70ee8bb0a58023bb05da7fdb12f3a
Kevin Isom <kevin.isom@gmail.com>
2013-10-14 12:50:02 +1300

    added required packages

---
+++ index.js
@@ -0,0 +1,23 @@
+/*
+Request XML
+
+<GenerateRequest>
+  <PxPayUserId>TestAccount</PxPayUserId>
+  <PxPayKey>dc339b3126c8fbadf4b30b498ded6a62a17b5f831e3111116bd8e332c730bbc8</PxPayKey>
+  <AmountInput>2.06</AmountInput>
+  <CurrencyInput>NZD</CurrencyInput>
+  <MerchantReference>Test Transaction</MerchantReference>
+  <EmailAddress></EmailAddress>
+  <TxnData1>28 Grange Rd</TxnData1>
+  <TxnData2>Auckland</TxnData2>
+  <TxnData3>NZ</TxnData3>
+  <TxnType>Purchase</TxnType>
+  <TxnId>P777575CA3DDA78C</TxnId>
+  <BillingId></BillingId>
+  <EnableAddBillCard>0</EnableAddBillCard>
+  <UrlSuccess>http://www.mycompany.com/success.cfm</UrlSuccess>
+  <UrlFail>http://www.mycompany.com/fail.cfm</UrlFail>
+  <Opt>TO=0901142221</Opt>
+</GenerateRequest>
+*/
+
