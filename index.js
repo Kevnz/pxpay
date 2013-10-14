f60233a966c1de00b6980315b7373a406b682368
Kevin Isom <kevin.isom@gmail.com>
2013-10-14 15:18:04 +1300

    first cut of request start

--- index.js
+++ index.js
@@ -1,23 +1,64 @@
-/*
-Request XML
+var request = require('request');
+var xml = require('xml');
+var url = 'https://sec.paymentexpress.com/pxaccess/pxpay.aspx';
 
-<GenerateRequest>
-  <PxPayUserId>TestAccount</PxPayUserId>
-  <PxPayKey>dc339b3126c8fbadf4b30b498ded6a62a17b5f831e3111116bd8e332c730bbc8</PxPayKey>
-  <AmountInput>2.06</AmountInput>
-  <CurrencyInput>NZD</CurrencyInput>
-  <MerchantReference>Test Transaction</MerchantReference>
-  <EmailAddress></EmailAddress>
-  <TxnData1>28 Grange Rd</TxnData1>
-  <TxnData2>Auckland</TxnData2>
-  <TxnData3>NZ</TxnData3>
-  <TxnType>Purchase</TxnType>
-  <TxnId>P777575CA3DDA78C</TxnId>
-  <BillingId></BillingId>
-  <EnableAddBillCard>0</EnableAddBillCard>
-  <UrlSuccess>http://www.mycompany.com/success.cfm</UrlSuccess>
-  <UrlFail>http://www.mycompany.com/fail.cfm</UrlFail>
-  <Opt>TO=0901142221</Opt>
-</GenerateRequest>
-*/
+var txtTypes = ['Purchase', 'Auth', 'Complete', 'Refund', 'Validate'];
+var SUCCESS_STATUS = 1;
+var FAIL_STATUS = 0;
 
+module.exports = {
+    request: function (details, callback) {
+        var dpsData = {
+            GenerateRequest: [
+                { PxPayUserId: details.user },
+                { PxPayKey: details.password },
+                { TxnType: details.transactionType || 'Purchase' },
+                { AmountInput: details.amount },
+                { CurrencyInput: details.currency || 'NZD' },
+                { MerchentReference: details.reference },
+                { TxnData1: details.line1 },
+                { TxnData2: details.line2 },
+                { TxnData3: details.line3 },
+                { EmailAddress: details.email },
+                { TxnId: details.transactionId || '' },
+                { BillingId: details.billingId },
+                { EnableAddBillCard: details.addCard },
+                { UrlSuccess: details.successURL },
+                { UrlFail: details.failURL }
+            ]
+        };
+
+        request({
+            uri: url,
+            method: 'POST',
+            body: xml(dpsData)
+        }, function (err, res, body) {
+        	console.log(body);
+            process.nextTick(function () {
+                if(err) {
+                    process.nextTick(function(){
+                        callback(err);
+                    });
+                } else {
+                    var parser = new require('xml2js').Parser({ explicitArray: false});
+                    process.nextTick(function(){
+                        parser.parseString(body, function (error, result){
+                            console.log(result);
+                            if(result.$.valid === 1){
+                                process.nextTick(function(){ callback(null, result); });
+                            } else if (error) {
+                                process.nextTick(function(){ callback(error); });
+                            } else {
+                                process.nextTick(function(){ callback({message: "result did not return correct code", result: result });});
+                            }
+                        });
+                    });
+                }
+
+            });
+        });
+    },
+    process: function () {
+        
+    }
+};
