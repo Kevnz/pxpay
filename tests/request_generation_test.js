579d620548ef5005bf09f2fc03886f8a0c2eb23e
Kevin Isom <kevin.isom@gmail.com>
2013-11-24 14:33:45 +1300

    working on the invoices

--- tests/request_generation_test.js
+++ tests/request_generation_test.js
@@ -30,6 +30,28 @@ describe('requestGeneration', function() {
         assert.equal(addBill, 1, "The Node value should be 1");
         done();
     });
+    it('request should have optional parameters', function (done) {
+        var options ={
+            user: config.get('user'),
+            password: config.get('password'),
+            amount: '1.00',
+            reference: 'Test',
+            line1: '1 Street Rd',
+            line2: 'Some Suburb',
+            line3: 'Testville',
+            email: 'test@example.com',
+            TxnId: 'test-'+Date.now(),
+            addCard: 1,
+            successURL: 'http://example.com/success',
+            failURL: 'http://example.com/fail'
+        };
+        var rq = pxpay.generateRequest(options);
+        console.log(rq);
+        var doc = new dom().parseFromString(rq);
+        var addref = xpath.select("/GenerateRequest/MerchentReference/text()", doc).toString();
+        assert.equal(addref, 'Test', "The Node value should be Test");
+        done();
+    });
 
 
 
