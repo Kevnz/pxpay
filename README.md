e34cad7bc684279187751139217b1a081353bbdc
Kevin Isom <kevin.isom@gmail.com>
2019-02-19 08:33:53 +1300

    Update deps, versions to test and eslint style

--- README.md
+++ README.md
@@ -1,32 +1,34 @@
 [![Build Status](https://travis-ci.org/Kevnz/pxpay.png?branch=master)](https://travis-ci.org/Kevnz/pxpay)
 
-## pxpay 
+# PxPay
 
-### PxPay implementation for node.js
+PxPay implementation for node.js
 
 Node module to use the [PxPay](http://paymentexpress.co.nz/developer-e-commerce-paymentexpress-hosted-pxpay) service from [Payment Express](http://paymentexpress.co.nz/).
 
 #### Install
 ```bash
-$ npm install pxpay --save
+$ npm install pxpay
 ```
 
 #### Usage
 
 ```javascript
-var pxpay = require('pxpay');
-pxpay.request({
-    user: 'TestAccount',
-    password: 'password',
-    amount: '1.00',
-    reference: 'Test',
-    transactionId: 'test-' + Date.now(),
-    addCard: 1,
-    successURL: 'http://example.com/success',
-    failURL: 'http://example.com/fail'
+const pxpay = require('pxpay')
+  pxpay.request({
+  user: 'TestAccount',
+  password: 'password',
+  amount: '1.00',
+  reference: 'Test',
+  transactionId: 'test-' + Date.now(),
+  addCard: 1,
+  successURL: 'http://example.com/success',
+  failURL: 'http://example.com/fail'
 }, function submitcallback (err, result) {
-
-    result.$.valid; //=== 1
-
-});
+  result.$.valid; //=== 1
+})
 ```
+
+## Notes
+
+If you are running in production it uses the Payment Express production URL, otherwise it uses the UAT url.
--- index.js
+++ index.js
@@ -1,98 +1,118 @@
-
-var xml = require('xml');
-var url = 'https://sec.paymentexpress.com/pxaccess/pxpay.aspx';
-
-var txtTypes = ['Purchase', 'Auth', 'Complete', 'Refund', 'Validate'];
-var SUCCESS_STATUS = 1;
-var FAIL_STATUS = 0;
+const xml = require('xml')
+const Parser = require('xml2js').Parser
+const prod = 'https://sec.paymentexpress.com/pxaccess/pxpay.aspx'
+const uat = 'https://uat.paymentexpress.com/pxaccess/pxpay.aspx'
+const txtTypes = ['Purchase', 'Auth', 'Complete', 'Refund', 'Validate']
+const SUCCESS_STATUS = 1
+const FAIL_STATUS = 0
 
 module.exports = {
-    generateRequest: function (details) {
-        var dataPayload = [
-            { PxPayUserId: details.user },
-            { PxPayKey: details.password },
-            { TxnType: details.transactionType || 'Purchase' },
-            { AmountInput: details.amount },
-            { CurrencyInput: details.currency || 'NZD' }
-        ];
-        if (details.successURL ) {
-            dataPayload.push( { UrlSuccess: details.successURL });
-        }
-        if (details.failURL ) {
-            dataPayload.push( {  UrlFail: details.failURL });
-        }
-        if (details.TxnId) {
-            dataPayload.push({ TxnId: details.TxnId });
-        }
-        if (details.transactionId) {
-            dataPayload.push({ TxnId: details.transactionId });
-        }
-        if (details.reference) {
-            dataPayload.push({ MerchantReference: details.reference });
-        }
-        if (details.email) {
-            dataPayload.push({ EmailAddress: details.email });
-        }
-        if (details.line1) {
-            dataPayload.push({ TxnData1: details.line1 });
-        }
-        if (details.line2) {
-            dataPayload.push({ TxnData2: details.line2 });
-        }
-        if (details.line3) {
-            dataPayload.push({ TxnData3: details.line3 });
-        }
-        if (details.billingId) {
-            dataPayload.push({ BillingId: details.billingId });
-        }
-        if (details.addCard) {
-            dataPayload.push({ EnableAddBillCard: details.addCard });
-        }
-        if (details.dpsBillingId) {
-           dataPayload.push({ DpsBillingId: details.dpsBillingId });
-        }
-        var dpsData = {
-            GenerateRequest: dataPayload
-        };
-        return xml(dpsData);
-    },
-    request: function (details, callback) {
-        var rquest = require('request');
-        var dpsData = this.generateRequest(details);
-        var self = this;
-        rquest({
-            uri: url,
-            method: 'POST',
-            body: dpsData
-        }, function requestCallback (err, res, body) {
-            process.nextTick(function() {
-                self.process(err, res, body, callback);
-            });
-        });
-    },
-    process: function (err, res, body, callback) {
-        if (err) {
-            process.nextTick(function(){
-                callback(err);
-            });
-        } else {
-            var parser = new require('xml2js').Parser({ explicitArray: false});
-
-            process.nextTick(function(){
-                parser.parseString(body, function parserHandler (error, result){
-                    if (error) {
-                        process.nextTick(function(){ callback(error); });
-                    } else if (!result || !result.Request || !result.Request.$ || !result.Request.$.valid) {
-                        process.nextTick(function() {
-                            callback({message: "result did not return correct code", result: result });
-                        });
-                    } else if(result.Request.$.valid === '1') {
-                        process.nextTick(function(){ callback(null, result.Request); });
-                    } else {
-                        process.nextTick(function(){ callback({message: "result did not return correct code", result: result });});
-                    }
-                });
-            });
-        }
+  generateRequest: function(details) {
+    const dataPayload = [
+      { PxPayUserId: details.user },
+      { PxPayKey: details.password },
+      { TxnType: details.transactionType || 'Purchase' },
+      { AmountInput: details.amount },
+      { CurrencyInput: details.currency || 'NZD' },
+    ]
+    if (details.successURL) {
+      dataPayload.push({ UrlSuccess: details.successURL })
     }
-};
+    if (details.failURL) {
+      dataPayload.push({ UrlFail: details.failURL })
+    }
+    if (details.TxnId) {
+      dataPayload.push({ TxnId: details.TxnId })
+    }
+    if (details.transactionId) {
+      dataPayload.push({ TxnId: details.transactionId })
+    }
+    if (details.reference) {
+      dataPayload.push({ MerchantReference: details.reference })
+    }
+    if (details.email) {
+      dataPayload.push({ EmailAddress: details.email })
+    }
+    if (details.line1) {
+      dataPayload.push({ TxnData1: details.line1 })
+    }
+    if (details.line2) {
+      dataPayload.push({ TxnData2: details.line2 })
+    }
+    if (details.line3) {
+      dataPayload.push({ TxnData3: details.line3 })
+    }
+    if (details.billingId) {
+      dataPayload.push({ BillingId: details.billingId })
+    }
+    if (details.addCard) {
+      dataPayload.push({ EnableAddBillCard: details.addCard })
+    }
+    if (details.dpsBillingId) {
+      dataPayload.push({ DpsBillingId: details.dpsBillingId })
+    }
+    var dpsData = {
+      GenerateRequest: dataPayload,
+    }
+    return xml(dpsData)
+  },
+  request: function(details, callback) {
+    var rquest = require('request')
+    var dpsData = this.generateRequest(details)
+    var self = this
+    rquest(
+      {
+        uri: process.env.NODE_ENV === 'production' ? prod : uat,
+        method: 'POST',
+        body: dpsData,
+      },
+      function requestCallback(err, res, body) {
+        process.nextTick(function() {
+          self.process(err, res, body, callback)
+        })
+      }
+    )
+  },
+  process: function(err, res, body, callback) {
+    if (err) {
+      process.nextTick(function() {
+        callback(err)
+      })
+    } else {
+      var parser = new Parser({ explicitArray: false })
+
+      process.nextTick(function() {
+        parser.parseString(body, function parserHandler(error, result) {
+          if (error) {
+            process.nextTick(function() {
+              callback(error)
+            })
+          } else if (
+            !result ||
+            !result.Request ||
+            !result.Request.$ ||
+            !result.Request.$.valid
+          ) {
+            process.nextTick(function() {
+              callback({
+                message: 'result did not return correct code',
+                result: result,
+              })
+            })
+          } else if (result.Request.$.valid === '1') {
+            process.nextTick(function() {
+              callback(null, result.Request)
+            })
+          } else {
+            process.nextTick(function() {
+              callback({
+                message: 'result did not return correct code',
+                result: result,
+              })
+            })
+          }
+        })
+      })
+    }
+  },
+}
